5ba07e5fdd125c2b8cd3338e34c4f5cc3b019461
Dmitry Kazaev <dmytrokazaev@gmail.com>
2016-11-04 11:28:52 +0200

    test study

---
+++ src/app/components/test/TestComp.js
@@ -0,0 +1,18 @@
+import React, { PropTypes as pt, Component } from 'react'
+
+export class TestComp extends Component {
+  // constructor(props) {
+  //   super(props);
+  //
+  // }
+
+
+  render() {
+
+    return (
+      <div className='myTestDiv'>
+        Arr
+      </div>
+    )
+  }
+}
